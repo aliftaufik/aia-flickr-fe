dda91ccafe749f5e773365ac22fe02657a68cfbd
Alif Taufik <alif.tr.aminuddin@gmail.com>
2021-10-05 16:55:14 +0700

    First Initialisation - Initialize react app using cra - Configure tailwindcss setup (installing tailwindcss dependencies, updating scripts to use craco, use jit config for tailwindcss)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="bg-blue-600 text-white">
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
