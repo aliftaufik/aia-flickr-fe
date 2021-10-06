2a500a1efc7770156141cb95cb873299384f1701
Alif Taufik <alif.tr.aminuddin@gmail.com>
2021-10-06 12:16:12 +0700

    Update Layout - Update Carousel; add zoom capabilities, add button to redirect user to flickr website - Update App.js layout - Update package start script as tailwind JIT mode need an environment variable to be set

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ function App() {
   }, []);
 
   return (
-    <div className="text-white">
-      <header>
-        <h1>Flickr Feeds</h1>
+    <div className="h-screen">
+      <header className="text-center mt-6">
+        <h1 className="text-xl font-bold">Flickr Feeds</h1>
+        <p>
+          For you who really have nothing to do in life, just look at theese
+          images to make you feel a bit better.
+        </p>
       </header>
 
-      <Carousel imageList={imageList}></Carousel>
+      <Carousel
+        imageList={imageList}
+        className="mt-6 max-w-screen-lg mx-auto"
+      ></Carousel>
     </div>
   );
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,28 +1,63 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation, Pagination } from "swiper";
+import SwiperCore, { Navigation, Pagination, Zoom } from "swiper";
+import { useMemo, useState } from "react";
 
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/zoom/zoom.min.css";
 
-SwiperCore.use([Navigation, Pagination]);
+SwiperCore.use([Navigation, Pagination, Zoom]);
 
-export default function Carousel({ imageList = [] }) {
+export default function Carousel({ imageList = [], className, ...props }) {
+  const [swiperController, setSwiperController] = useState(null);
+
+  const handleMouseEnter = useMemo(() => {
+    if (!swiperController) return null;
+    return () => {
+      swiperController.zoom.in();
+    };
+  }, [swiperController]);
+
+  const handleMouseLeave = useMemo(() => {
+    if (!swiperController) return null;
+    return () => {
+      swiperController.zoom.out();
+    };
+  }, [swiperController]);
+
   return (
-    <Swiper
-      navigation
-      pagination={{ clickable: true, dynamicBullets: true }}
-      loop
-    >
-      {imageList.map((image, index) => (
-        <SwiperSlide key={index}>
-          <img
-            className="h-96 mx-auto mb-6"
-            src={image.image}
-            alt="Feeds"
-          ></img>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className={"relative h-[520px] " + className} {...props}>
+      <Swiper
+        navigation
+        pagination={{ clickable: true, dynamicBullets: true }}
+        loop
+        zoom={{ maxRatio: 2 }}
+        onSwiper={setSwiperController}
+        className="h-full"
+      >
+        {imageList.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className="swiper-zoom-container">
+              <img
+                className="h-full mx-auto mb-12"
+                src={image.image}
+                alt="Feeds"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              ></img>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="absolute z-10 inset-x-0 bottom-8 flex justify-center items-center">
+        <a
+          href="flickr.com"
+          className="rounded px-4 py-2 bg-gray-700 transition-opacity opacity-20 hover:opacity-100 text-white"
+        >
+          See High Res Version
+        </a>
+      </div>
+    </div>
   );
 }
